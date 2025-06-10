04a9f53e9901a6c8fa41bf329e9fc899b48240d6
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2025-06-10 11:39:36 +0200

    ajout fichier main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Ceci est un commentaire
+
+/*
+Ceci est un commentaire sur plusieurs lignes
+*/
+
+// Ceci est un commentaire sur une ligne
+
+// Ceci est un commentaire sur une ligne
+
+// Ceci est un commentaire sur une ligne
